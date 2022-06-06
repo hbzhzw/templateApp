a43d0d7d0a9fd11a95234bfb23f49a8555933d2f
hzhang <hzhang@tencent.com>
2022-06-06 22:15:05 +0800

    feat: add screen util dptopx

--- utils/src/main/java/io/github/hbzhzw/utils/system/ScreenUtil.kt
+++ utils/src/main/java/io/github/hbzhzw/utils/system/ScreenUtil.kt
@@ -1,7 +1,13 @@
 package io.github.hbzhzw.utils.system
 
-class ScreenUtil {
-    companion object {
+import io.github.hbzhzw.utils.AppContext
 
+object ScreenUtil {
+    fun dpToPx(dp: Int): Int {
+        return (AppContext.context.resources.displayMetrics.density * dp + .5f).toInt()
+    }
+
+    fun pxToDp(pixel: Int): Int {
+        return ((pixel.toFloat() / AppContext.context.resources.displayMetrics.density) + 0.5f).toInt()
     }
 }
