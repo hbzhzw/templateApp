1016d41a1a5b78e33b74069a46597641291ab946
hzhang <hzhang@tencent.com>
2023-05-05 10:29:51 +0800

    feat: adjust title home

--- utils/src/main/java/io/github/hbzhzw/utils/common/ByteUtils.kt
+++ utils/src/main/java/io/github/hbzhzw/utils/common/ByteUtils.kt
@@ -1,7 +1,6 @@
 package io.github.hbzhzw.utils.common
 
 object ByteUtils {
-    private const val TAG = "ByteUtils"
     private val HEX_CHAR = listOf('0', '1', '2', '3', '4', '5', '6',
         '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F')
 
